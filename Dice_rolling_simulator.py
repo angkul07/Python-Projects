7610d3fe780ef0502d1485d0ae5852f583b55406
angkul07 <angkul58@gmail.com>
2023-09-07 11:14:10 +0530

    play sound when the dice rollusing playsound module

--- Dice_rolling_simulator.py
+++ Dice_rolling_simulator.py
@@ -1,5 +1,8 @@
 import random
+from playsound import playsound
 
 roll = input("Hit the enter to roll the dice: ")
+# Adding sound using playsound when the dice roll
+playsound('dice.mp3')
 number = [1, 2, 3, 4, 5, 6]
-print(random.choice(number))
+print(f"You got the number: {random.choice(number)}")
