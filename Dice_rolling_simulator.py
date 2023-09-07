d2e0f0e5bef72acc34df72bb25427e02bcb40938
angkul07 <angkul58@gmail.com>
2023-09-07 10:51:48 +0530

    Complete the basic version of dice rolling

---
+++ Dice_rolling_simulator.py
@@ -0,0 +1,5 @@
+import random
+
+roll = input("Hit the enter to roll the dice: ")
+number = [1, 2, 3, 4, 5, 6]
+print(random.choice(number))
---
+++ random_num_generator.py
@@ -0,0 +1,8 @@
+import random
+
+# Printing the random number for a variable
+num = random.random()
+print(num)
+
+list = [1, 3, 5, 'hello', 4.5]
+print(random.choice(list))
