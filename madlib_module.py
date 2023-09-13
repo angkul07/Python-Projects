53d360464f0121f469bedcfaea747515a8b8c53a
angkul07 <angkul58@gmail.com>
2023-09-13 15:16:36 +0530

    Module version of madlib

---
+++ madlib_module.py
@@ -0,0 +1,32 @@
+import random
+
+
+def realMadlib():
+    a = "[Name] had a pet [animal] named [pet_name]. One day, they went on a(n) [adjective1] adventure together. [Name] decided to cook [food] for dinner, but they accidentally added [weird_ingredient]. The meal turned out [adjective2]!"
+
+    b = "On a sunny [day_of_the_week], [Name] went to the [noun] and found a mysterious [object]. They decided to take it home and soon discovered that it had the power to [verb] anything!"
+
+    madlibList = [a, b]
+    list = random.choice(madlibList)
+    print(list)
+
+    if list == a:
+        name = input("Name: ")
+        animal = input("Animal: ")
+        pet_name = input("Pet name: ")
+        adjective1 = input("Adjective 1: ")
+        Name = name
+        food = input("Food: ")
+        weird_ingredient = input("Weird Ingredient: ")
+        adjective2 = input("Adjective 2: ")
+
+        print(f"{name} had a pet {animal} named {pet_name}. One day, they went on a(n) {adjective1} adventure together. {Name} decided to cook {food} for dinner, but they accidentally added {weird_ingredient}. The meal turned out {adjective2}!")
+
+    elif list == b:
+        day_of_the_week = input("Day of the week: ")
+        Name = input("Name: ")
+        noun = input("Noun: ")
+        object = input("Object: ")
+        verb = input("Verb: ")
+
+        print(f"On a sunny {day_of_the_week}, {Name} went to the {noun} and found a mysterious {object}. They decided to take it home and soon discovered that it had the power to {verb} anything!")
